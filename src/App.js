0784d0a5327eedc1d36124fb57aac04271902bfe
타래 <bae94157617@gmail.com>
2021-11-27 16:31:23 +0900

    1127

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import Button from "./Button";
+import { useEffect, useState } from "react";
 import styles from "./App.module.css";
+import style from "./Btn.module.css"
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(()=>{
+    console.log("난 한번만 실행 됨");
+  },[]);
+  useEffect(()=>{
+    console.log("Count마다 실행 됨");
+  },[counter]);
+  useEffect(()=>{
+    console.log("Key Input마다 실행 됨");
+  },[keyword]);
   return(
     <div>
-      <h1 className={styles.title}>Welcome React Class!!</h1>
-      <Button text={"Click!"} />
+      <input 
+        value={keyword} 
+        onChange={onChange} 
+        type="text" 
+        placeholder="Input value" 
+      />
+      <h1 className={styles.title}>{counter}</h1>
+      <button className={style.btn} onClick={onClick}>Click!</button>
     </div>
   );
 }
+
 export default App;
