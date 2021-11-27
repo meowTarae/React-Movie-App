6a7b11497d706a6fd8eb7ff8932bc120d96be221
타래 <bae94157617@gmail.com>
2021-11-27 16:37:19 +0900

    1227_16:37

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
-import { useEffect, useState } from "react";
-import styles from "./App.module.css";
-import style from "./Btn.module.css"
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/home";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(()=>{
-    console.log("난 한번만 실행 됨");
-  },[]);
-  useEffect(()=>{
-    console.log("Count마다 실행 됨");
-  },[counter]);
-  useEffect(()=>{
-    console.log("Key Input마다 실행 됨");
-  },[keyword]);
-  return(
-    <div>
-      <input 
-        value={keyword} 
-        onChange={onChange} 
-        type="text" 
-        placeholder="Input value" 
-      />
-      <h1 className={styles.title}>{counter}</h1>
-      <button className={style.btn} onClick={onClick}>Click!</button>
-    </div>
+  return (
+    <Router>
+      <Switch>
+        {/* switch는 route(= url)를 찾아주는 역활 */}
+        <Route path="/">
+          {/* Home Route를 render 해줌 */}
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
